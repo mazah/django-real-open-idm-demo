d860f62b41acfeab2d685c762469f3cf430f54cc
Markus <28872014+markusleh@users.noreply.github.com>
2021-04-08 14:20:22 +0300

    Modified CI

--- idmdemo/idmdemo/test_settings.py
+++ idmdemo/idmdemo/test_settings.py
@@ -15,7 +15,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        'TEST_NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': ':memory:',
     }
 }
