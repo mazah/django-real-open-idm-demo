50e6103674aa7dbcd3fc5f83cb9612e70c492aed
Markus <28872014+markusleh@users.noreply.github.com>
2021-04-08 13:53:10 +0300

    Modified CI

--- idmdemo/idmdemo/test_settings.py
+++ idmdemo/idmdemo/test_settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-1c0j8auewfm#pazumw0(u(@r#8t!qdo*^6n_qmx+o1n_d=no&g'
@@ -8,7 +9,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.environ.get('TRAVIS_BUILD_DIR')
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
