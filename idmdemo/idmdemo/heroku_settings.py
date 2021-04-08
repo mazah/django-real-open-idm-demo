ca376d1f6f6ab69bc06ba7947214e7e471cbb397
Markus <28872014+markusleh@users.noreply.github.com>
2021-04-08 21:33:14 +0300

    Added secret key for heroku

--- idmdemo/idmdemo/heroku_settings.py
+++ idmdemo/idmdemo/heroku_settings.py
@@ -5,6 +5,8 @@ import django_heroku
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+SECRET_KEY = os.getenv('SECRET_KEY', 'gjipowgeiofhiopchmpc89nc89ttchnp3')
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
