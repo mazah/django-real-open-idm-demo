6066cc9b178fdfa39c6734200bfde542bb1069ca
Markus <28872014+markusleh@users.noreply.github.com>
2021-04-08 14:04:53 +0300

    Modified CI

--- idmdemo/idmdemo/test_settings.py
+++ idmdemo/idmdemo/test_settings.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-import os
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-1c0j8auewfm#pazumw0(u(@r#8t!qdo*^6n_qmx+o1n_d=no&g'
@@ -9,14 +8,14 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-BASE_DIR = os.environ.get('TRAVIS_BUILD_DIR')
+BASE_DIR = Path(__file__).resolve().parent.parent
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'djagorealidm/db.sqlite3'),
-        'TEST_NAME': os.path.join(BASE_DIR, 'djagorealidm/db.sqlite3'),
+        'NAME': BASE_DIR / 'db.sqlite3',
+        'TEST_NAME': BASE_DIR / 'db.sqlite3',
     }
 }
